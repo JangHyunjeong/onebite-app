dffa479bb6f461962c3eaa3db863b093441cdd5a
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2025-07-09 23:58:10 +0900

    refactor: 추천 도서 호출 최적화 - revalidate 옵션 적용

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -20,7 +20,8 @@ async function AllBooks() {
 
 async function RecoBooks() {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
   );
   if (!response.ok) return <div>오류가 발생했습니다.</div>;
   const recoBooks: BookData[] = await response.json();
