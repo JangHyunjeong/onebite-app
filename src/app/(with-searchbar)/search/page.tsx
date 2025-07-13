e6b9dc735fa1c6f82980549bd06158804d0ce212
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2025-07-13 15:51:04 +0900

    refactor: 페이지 스트리밍 적용 및 테스트 함수 추가

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,5 +1,6 @@
 import BookItem from "@/components/book-item";
 import { BookData } from "@/types";
+import { delay } from "@/util/delay";
 
 export default async function Page({
   searchParams,
@@ -9,6 +10,7 @@ export default async function Page({
   const { q } = await searchParams;
 
   // 한번 검색이 된 결과에 대해서는 캐싱되도록 설정
+  await delay(1500);
   const response = await fetch(
     `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`,
     { cache: "force-cache" }
