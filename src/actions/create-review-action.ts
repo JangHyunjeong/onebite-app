f9836f76bc5be31ad3c19617d718e5d689c7e88f
JangHyunjeong <jang.hyunjeong01@gmail.com>
2025-07-18 14:14:15 +0900

    refactor: 리뷰 작성 완료 시, 리뷰 리스트 업데이트 (revalidatePath 추가)

--- src/actions/create-review-action.ts
+++ src/actions/create-review-action.ts
@@ -1,4 +1,7 @@
 "use server";
+
+import { revalidatePath } from "next/cache";
+
 // 이 함수는 server action으로 선언됨
 
 export async function createReviewAction(formData: FormData) {
@@ -15,6 +18,11 @@ export async function createReviewAction(formData: FormData) {
         body: JSON.stringify({ bookId, content, author }),
       }
     );
+    revalidatePath(`/book/${bookId}`);
+    // 해당 path에 해당하는 페이지를 서버에서 다시 검증후 렌더
+    // 서버측에서만 작동하는 함수이므로, 클라이언트에서 이 함수를 호출 할 수 없음
+    // 특정 페이지를 전부 재검증, 재생성 하기에 페이지의 모든 캐시 무효화 -> 페이지 캐시 옵션 무효됨
+    // + 풀라우트 캐시도 삭제함 / 업데이트된 정보를 반영하진 않음
   } catch (error) {
     console.error("error", error);
     return;
